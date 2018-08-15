5f615ebcd43f238d22058320798a4ec3c2f7ce46
Giovanny Melo <melo.giovanny153@gmail.com>
2018-08-15 15:50:07 -0300

    0.0.3
    Criação do navbar com ícones

--- index.php
+++ index.php
@@ -12,7 +12,19 @@
     </head>
 
     <body>
-      <nav></nav>
+      <nav class="blue-grey">
+        <div class = "nav-wrapper container">
+          <div class ="brand-logo light"> Cad Cli</div>
+          <ul class="right">
+            <li><a href=""><i class="material-icons left">account_box</i>Cadastro</a></li>
+            <li><a href=""><i class="material-icons left">find_in_page</i>Consultas</a></li>
+            <li><a href=""><i class="material-icons left">monetization_on
+</i>Financeiro</a></li>
+            <li><a href=""><i class="material-icons left">dvr</i>Relatório</a></li>
+            <li><a href=""><i class="material-icons left">home</i>Imóveis</a></li>
+          </ul>        
+        </div>
+      </nav>
 
       <!--JavaScript at end of body for optimized loading-->
       <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
