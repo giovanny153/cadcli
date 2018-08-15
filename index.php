163ed5f154484f26b672366a8c892d6eae2f981e
Giovanny Melo <melo.giovanny153@gmail.com>
2018-08-15 15:40:17 -0300

    0.0.2
    - Configuração do index com scripts e injections do materialize

--- index.php
+++ index.php
@@ -12,8 +12,14 @@
     </head>
 
     <body>
+      <nav></nav>
 
       <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){});
+    </script>
+
     </body>
   </html>
