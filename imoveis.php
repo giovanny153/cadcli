3ff3a69f014395d16057f25cf837daec2af56c40
Giovanny Melo <melo.giovanny153@gmail.com>
2018-08-17 14:47:24 -0300

    0.2.4
    - Implementação do Banner
    - Separação dos arquivos da página de imoveis em includes

--- imoveis.php
+++ imoveis.php
@@ -3,24 +3,6 @@ session_start()
 ?>
 <?php include_once 'includes/header.inc.php'?>
 <?php include_once 'includes/menu.inc.php'?>  
-
-
-<div class="row container">
-  <p>&nbsp;</p>
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="imgs/casa.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-    </div>
-  </div>
-  
-</div>
-
+<?php include_once 'includes/banner.inc.php'?>
+<?php include_once 'includes/imoveis.inc.php'?>
 <?php include_once 'includes/footer.inc.php' ?>
