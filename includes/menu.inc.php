ccaa9a2e5460cf22bdd513c4d8d6d29089f73f98
Giovannny Melo <melo.giovanny153@gmail.com>
2018-08-18 14:22:29 -0300

    0.2.8
    - inclusão botão de listas para arquivos read

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -4,11 +4,17 @@
         <div class = "nav-wrapper container">
           <div class ="brand-logo light"> Cad Cli</div>
           <ul class="right">            
+            
+            <li><a href="imoveis.php" class="btn waves-effect"><i class="material-icons">home</i></a></li>
+            
+            <li><a class="dropdown-trigger btn waves-effect" data-target="dropdown1"><i class="material-icons">format_list_bulleted</i></a></li>
+
+            <li><a class="dropdown-trigger btn waves-effect" data-target="dropdown2"><i class="material-icons">trending_up</i></a></li>
+
+            <li><a class="dropdown-trigger btn waves-effect" data-target="dropdown3"><i class="material-icons">add_to_photos</i></a></li>
+
             <!-- <li><a href="cli_consultas.php"><i class="material-icons left">find_in_page</i>Consultas</a></li>
             <li><a href="cli_consultas.php"><i class="material-icons left">find_in_page</i>Venda Cliente</a></li> -->
-            <li><a href="imoveis.php" class="btn waves-effect"><i class="material-icons">home</i></a></li>
-            <li><a class="dropdown-trigger btn waves-effect" data-target="dropdown2"><i class="material-icons">trending_up</i></a></li>
-            <li><a class="dropdown-trigger btn waves-effect" data-target="dropdown1"><i class="material-icons">add_to_photos</i></a></li>
             <!-- <li><a href="suporte.php"><i class="material-icons left">account_box</i>Suportes</a></li>
             <li><a href="consult.php"><i class="material-icons left">find_in_page</i>Consult Sup</a></li> -->
             <!-- <li><a href=""><i class="material-icons left">monetization_on</i>Financeiro</a></li>
@@ -18,14 +24,22 @@
         </div>
       </nav>
 
+
+
 <ul id="dropdown1" class="dropdown-content">
-  <li><a href="cli_cad.php">Cliente</li></a>
-  <li><a href="fin_cad.php">Fin</a></li>
-  <li><a href="#!">Imóvel</a></li>
+  <li><a href="cli_consultas.php">Cliente</li></a>
+  <li><a href="fin_consultas">Fin</a></li>
+  <li><a href="imov_consultas">Imóvel</a></li>
 </ul>
 
 <ul id="dropdown2" class="dropdown-content">
   <li><a href="cli_consultas.php">Cliente</li></a>
   <li><a href="fin_consultas">Fin</a></li>
   <li><a href="imov_consultas">Imóvel</a></li>
+</ul>
+
+<ul id="dropdown3" class="dropdown-content">
+  <li><a href="cli_cad.php">Cliente</li></a>
+  <li><a href="fin_cad.php">Fin</a></li>
+  <li><a href="#!">Imóvel</a></li>
 </ul>
